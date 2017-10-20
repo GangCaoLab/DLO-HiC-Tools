c4e34216eef26e17f0c1150d5bba7a120143e8ef
nanguage <nanguage@yahoo.com>
2017-10-20 10:27:04 +0800

    refine ice_adjust.py

--- dlo_hic/tools/ice_adjust.py
+++ dlo_hic/tools/ice_adjust.py
@@ -12,23 +12,22 @@ import sys
 import argparse
 
 import matplotlib
-matplotlib.use("AGG") # use AGG backend, avoid system don't have GUI.
 import matplotlib.pyplot as plt
 import numpy as np
 from iced import filter, normalization
 
-from hicmatrix import HicMatrix, HicChrMatrix
+from dlo_hic.IO.matrix import load_hicmat
+from dlo_hic import HicChrMatrix
 
 
 def argument_parser():
     parser = argparse.ArgumentParser(" Perform ICE(Iterative Correction and Eigenvector decomposition) on hic result matrix.")
 
-    parser.add_argument("--input", "-i",
-            required=True,
-            help="prefix of input matrix files: .npy and [.pyobj] file")
+    parser.add_argument("input",
+            help="input matrix file(.npz) name")
 
-    parser.add_argument("--output", "-o",
-            help="prefix of output files.")
+    parser.add_argument("output_prefix",
+            help="prefix of output file.")
 
     parser.add_argument("--percentage", "-p",
             type=float,
@@ -41,27 +40,32 @@ def argument_parser():
     return parser
 
 
-def main():
-    parser = argument_parser()
-    args = parser.parse_args()
-
-    hicmat = HicMatrix.load(args.input)
-    hicmat.matrix = hicmat.matrix.astype(np.float)
+def main(input, output_prefix, percentage, figure):
+    hicmat = load_hicmat(args.input)
+    hicmat.matrix =np.array( hicmat.matrix.astype(np.float))
     if isinstance(hicmat, HicChrMatrix):
-        filtered_mat = filter.filter_low_counts(hicmat.matrix,\
-                lengths=hicmat.lengths, percentage=args.percentage)
+        filtered_mat = filter.filter_low_counts(hicmat.matrix,
+                lengths=hicmat.lengths, percentage=percentage)
     else:
-        filtered_mat = filter.filter_low_counts(hicmat.matrix,\
-                percentage=args.percentage)
+        filtered_mat = filter.filter_low_counts(hicmat.matrix,
+                percentage=percentage)
     normed_mat = normalization.ICE_normalization(filtered_mat)
     hicmat.matrix = normed_mat
 
-    HicMatrix.save(hicmat, args.output)
+    hicmat.save(output_prefix)
 
-    if args.figure:
+    if figure:
         hicmat.plot()
-        plt.savefig(args.figure, dpi=500)
+        plt.savefig(figure, dpi=600)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argument_parser()
+    args = parser.parse_args()
+
+    input = args.input
+    output_prefix = args.output_prefix
+    percentage = args.percentage
+    figure = args.figure
+
+    main(input, output_prefix, percentage, figure)
