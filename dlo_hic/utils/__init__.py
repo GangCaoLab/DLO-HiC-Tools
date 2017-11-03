b50dc814f3908d2500314b3ed4b1b616a71adb06
nanguage <nanguage@yahoo.com>
2017-11-03 15:35:52 +0800

    add bwa_wrap

--- dlo_hic/utils/__init__.py
+++ dlo_hic/utils/__init__.py
@@ -1,1 +1,2 @@
 from .reverse_complement import reverse_complement
+from .bwa_wrap import BWA
---
+++ dlo_hic/utils/bwa_wrap.py
@@ -0,0 +1,34 @@
+import os
+import subprocess
+import multiprocessing
+
+cores = multiprocessing.cpu_count()
+
+class BWA():
+    @classmethod
+    def index(cls, ref_fasta, index_prefix):
+        suffix = ["amb", "bwt", "sa", "ann", "pac"]
+        files = [index_prefix + "." + s for s in suffix]
+        exists = [os.path.exists(f) for f in files]
+        all_exists = all(exists)
+        if not all_exists:
+            cmd = "bwa index -p {} {}".format(index_prefix, ref_fasta)
+            subprocess.check_call(cmd, shell=True)
+
+    def __init__(self, index_prefix):
+        self.index_prefix = index_prefix
+
+    def run(self, input, output_prefix, thread=cores, mem=False):
+        """ launch bwa aln/mem command, return process """
+        if mem:
+            aln_cmd = "bwa mem -t {} {} {} > {}.sam".format(
+                thread, self.index_prefix, input, output_prefix)
+            subprocess.check_call(aln_cmd, shell=True)
+        else:
+            aln_cmd = "bwa aln -t {} {} {} > {}.sai".format(
+                thread, self.index_prefix, input, output_prefix)
+            subprocess.check_call(aln_cmd, shell=True)
+            trans_cmd = "bwa samse {} {}.sai {} > {}.sam".format(
+                self.index_prefix, output_prefix, input, output_prefix)
+            subprocess.check_call(trans_cmd, shell=True)
+            os.remove(output_prefix+'.sai')
