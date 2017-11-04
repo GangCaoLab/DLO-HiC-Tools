efbcd1bca949268cd26b7c41fd613e240acd886f
nanguage <nanguage@yahoo.com>
2017-11-04 16:23:55 +0800

    add read_args function to utils

--- dlo_hic/utils/__init__.py
+++ dlo_hic/utils/__init__.py
@@ -1,2 +1,7 @@
 from .reverse_complement import reverse_complement
 from .bwa_wrap import BWA
+
+def read_args(args, scope):
+    for var in dir(args):
+        if not var.startswith("_"):
+            scope[var] = getattr(args, var)
