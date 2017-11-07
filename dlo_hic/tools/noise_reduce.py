3bc52f204bc09e14ab31ea53ca36d70e7da4353b
nanguage <nanguage@yahoo.com>
2017-11-07 11:21:33 +0800

    modify tools/noise_reduce.py

--- dlo_hic/tools/noise_reduce.py
+++ dlo_hic/tools/noise_reduce.py
@@ -18,57 +18,74 @@ so maybe there are few restriction sites within pair:
 normal:
                          many restriction site
              
-               left hooker     |  ...  |      right hooker
+                 left PET      |  ...  |        right PET
                     V          V       V            V     
     genome    <-+++..+++--..---*---..--*----..--+++..+++->
 
 abnormal(self-ligation-1):
-                        no or few restriction site
-    
-               left hooker     |              right hooker
+                     no or few restriction site
+
+                 left PET      |                right PET
                     V          V                    V     
     genome    <-+++..+++--..---*---..-------..--+++..+++->
 
 
 In second situation,
-left and right hookers will overlap like:
+left and right PETs will overlap like:
 
 abnormal(self-ligation-2):
             
-                              +++++++..++ right hooker
-             left hooker ++..+++++++
+                              +++++++..++ right PET
+             left PET ++..+++++++
     genome    <---..--------------------------------..--->
 
 Therefore, with the propose of clean the noise, here shold remove the pair,
 which there are no restriction site or just very few restriction(e.g. one)
-within it, or the left and right hookers overlapped.
+within it, or the left and right PETs overlapped.
 
 """
 
-
+from __future__ import print_function
+import os
 import sys
+import time
+import cPickle
 import argparse
+import signal
+from Queue import Empty
+import multiprocessing
+from multiprocessing import Queue, Process
+
+from intervaltree import Interval, IntervalTree
+
+from dlo_hic.utils import read_args
+from dlo_hic.utils.parse_text import parse_line_bed6, parse_line_bedpe
+from dlo_hic.utils.itree import build_bed6_itree, load_bed6_itree
+
+
+TIME_OUT = 1
+CHUNK_SIZE = 1000
 
 
 def argument_parser():
     parser = argparse.ArgumentParser(
             description="Remove DLO-HiC noise (self-ligation).")
 
-    parser.add_argument("--input", "-i",
-            type=argparse.FileType(mode="r"),
-            default=sys.stdin,
+    parser.add_argument("input",
             help="Input file, in bedpe file format.")
 
-    parser.add_argument("--output", "-o",
-            type=argparse.FileType(mode="w"),
-            default=sys.stdout,
+    parser.add_argument("output",
             help="Output file, default stdout.")
 
     parser.add_argument("--restriction", "-r",
             required=True,
-            type=argparse.FileType(mode="r"),
             help="bed file which recorded the position of all restriction sites"
                  "in the genome.")
+
+    parser.add_argument("--processes", "-p",
+            type=int,
+            default=1,
+            help="Use how many processes to run.")
 
     parser.add_argument("--threshold_num_rest", "-n",
             type=int,
@@ -89,107 +106,177 @@ def argument_parser():
 
 
 def load_rest_sites(sites_file):
-    """ load all restriction site """
-    rest_sites = {}
-    with sites_file as f:
-        for line in f:
-            line = line.strip()
-            chr_, start, end, _, _, strand = line.split("\t")
-            start, end = int(start), int(end)
-            assert start <= end
-            rest_sites.setdefault(chr_, list())
-            rest_sites[chr_].append((start, end))
+    """ load all restriction sites. """
+    itree_file = sites_file + '.itree'
+    if os.path.exists(itree_file):
+        rest_sites = load_bed6_itree(itree_file)
+    else:
+        rest_sites = build_bed6_itree(sites_file, itree_file)
     return rest_sites
 
 
 def find_interval_rests(span, chr_, rest_sites):
     """ find all rests within span. """
-    intervals = []
-    try:
-        rest_sites = rest_sites[chr_]    
-    except KeyError as e:
-        sys.stderr.write(e)
-        raise IOError("There are no chr: {} in resriction bed file.".format(chr_))
-    for start, end in rest_sites:
-        if start > span[0] and end < span[1]:
-            intervals.append((start, end))
+    itree = rest_sites[chr_]
+    start, end = span
+    intervals = itree[start:end]
     return intervals
 
 
-def stream_processing(file_in, file_out,
-        rest_sites,
-        threshold_num_rest,
-        threshold_span,
-        debug=False):
-    """ processing input file and output result """
-    with file_in as fi, file_out as fo:
-        for line in fi:
-            chr1, start1, end1, chr2, start2, end2 = line.strip().split("\t")[:6]
-            start1, end1, start2, end2 = [int(i) for i in 
-                    (start1, end1, start2, end2)]
-            if chr1 != chr2:
-                # inter chromalsomal interaction
-                fo.write(line)
+def read_chunk(file, chunk_size=CHUNK_SIZE):
+    """ read and parse bedpe file,
+    return a list of bedpe items
+    """
+    chunk = []
+    for i in range(chunk_size):
+        line = file.readline()
+        if not line:
+            if i == 0:
+                raise StopIteration
             else:
-                # inter chromalsomal interaction
-                if end1 < start2:
-                    """ 
-                              hooker1         hooker2
-                    <--...--s1--..--e1--..--s2--..--e2--..-->
-                    """
-                    if start2 - end1 > threshold_span:
-                        # safe span: interaction distance enough long
-                        fo.write(line)
-                    else:
-                        # unsafe span, need to be check
-                        interval_rests = find_interval_rests((end1, start2), chr1, rest_sites)
-                        if len(interval_rests) <= threshold_num_rest:
-                            fo.write(line)
-                        elif debug:
-                            sys.stderr.write("[abnormal-1]\t" + line)
-                elif end2 < start1:
-                    """ 
-                              hooker2        hooker1
-                    <--...--s2--..--e2--..--s1--..--e1--..-->
-                    """
-                    if start1 - end2 > threshold_span:
-                        # safe span: interaction distance enough long
-                        fo.write(line)
-                    else:
-                        # unsafe span, need to be check
-                        interval_rests = find_interval_rests((end2, start1), chr1, rest_sites)
-                        if len(interval_rests) <= threshold_num_rest:
-                            fo.write(line)
-                        elif debug:
-                            sys.stderr.write("[abnormal-1]\t" + line)
-                else:
-                    """ 
-                    overlap:
-
-                              hooker1         hooker2
-                    <--...--s1--..--s2--..--e1--..--e2--..-->
-
-                    et.al
-
-                    """
-                    if debug:
-                        sys.stderr.write("[abnormal-2]\t" + line)
-                    else:
-                        continue
+                return chunk
+        items = parse_line_bedpe(line)
+        chunk.append(items)
+    return chunk
 
 
-def main():
-    parser = argument_parser()
-    args = parser.parse_args()
-    # load restriction sites
-    rest_sites = load_rest_sites(args.restriction)
-    # processing data
-    stream_processing(args.input, args.output,
-            rest_sites,
-            args.threshold_num_rest,
-            args.threshold_span,
-            args.debug)
+def bedpe_type(rest_sites, bedpe_items):
+    """ judge interaction type,
+    types:
+        "normal"
+        "abnormal-1"
+        "abnormal-2"
+    """
+    chr1, start1, end1, chr2, start2, end2 = bedpe_items[:6]
+    # inter chromalsomal interaction
+    if chr1 != chr2:
+        return "normal"
+
+    # intra chromalsomal interaction
+    if end1 < start2:
+        """
+                  PET1            PET2
+        <--...--s1--..--e1--..--s2--..--e2--..-->
+        """
+        if start2 - end1 > threshold_span:
+            # safe span: interaction distance enough long
+            return "normal"
+        else:
+            # unsafe span, need to be check
+            interval_rests = find_interval_rests((end1, start2), chr1, rest_sites)
+            if len(interval_rests) <= threshold_num_rest:
+                return "normal"
+            else:
+                return "abnormal-1"
+    elif end2 < start1:
+        """
+                  PET2            PET1
+        <--...--s2--..--e2--..--s1--..--e1--..-->
+        """
+        if start1 - end2 > threshold_span:
+            # safe span: interaction distance enough long
+            return "normal"
+        else:
+            # unsafe span, need to be check
+            interval_rests = find_interval_rests((end2, start1), chr1, rest_sites)
+            if len(interval_rests) <= threshold_num_rest:
+                return "normal"
+            else:
+                return "abnormal-1"
+    else:
+        """
+        overlap:
+
+                  PET1            PET2
+        <--...--s1--..--s2--..--e1--..--e2--..-->
+
+        et.al
+
+        """
+        return "abnormal-2"
+
+
+def worker(task_queue, output_queue, err_queue, rest_sites):
+    while 1:
+        try:
+            chunk = task_queue.get(timeout=TIME_OUT)
+        except Empty:
+            break
+        out_chunk = []
+        err_chunk = []
+        for items in chunk:
+            type_ = bedpe_type(rest_sites, items)
+            if type_ == 'normal':
+                out_chunk.append(items)
+            else:
+                err_chunk.append((type_, items))
+        output_queue.put(out_chunk)
+        err_queue.put(err_chunk)
+
+
+def outputer(out_file, output_p):
+    f = open(out_file, 'w')
+    def signal_handeler(signal, frame):
+        f.close()
+        sys.exit(0) 
+    signal.signal(signal.SIGTERM, signal_handeler)
+    while 1:
+        chunk = output_p.get()
+        for items in chunk:
+            items = [str(i) for i in items]
+            line = "\t".join(items) + "\n"
+            f.write(line)
+
+
+def main(input, output,
+         restriction, processes,
+         threshold_num_rest, threshold_span,
+         debug):
+    print("loding restriction sites", file=sys.stderr)
+    rest_sites = load_rest_sites(restriction)
+    print("done")
+    task_queue = Queue()
+    output_queue = Queue()
+    err_queue = Queue()
+
+    workers = [Process(target=worker,
+                       args=(task_queue, output_queue, err_queue, rest_sites))
+               for i in range(processes)]
+
+    output_p = Process(target=outputer, args=(output, output_queue))
     
+    for w in workers:
+        w.start()
+
+    output_p.start()
+
+    with open (input) as f:
+        while 1:
+            try:
+                task_queue.put(read_chunk(f))
+            except StopIteration:
+                break
+
+    for w in workers:
+        w.join()
+    while not output_queue.empty():
+        time.sleep(TIME_OUT)
+    output_p.terminate()
+
+    if debug:
+        while not err_queue.empty():
+            chunk = err_queue.get()
+            for type_, items in chunk:
+                items = [str(i) for i in items]
+                line = "\t".join(items)
+                print("[{}]\t{}".format(type_, line), file=sys.stderr)
+
 
 if __name__ == "__main__":
-    main()
+    parser = argument_parser()
+    args = parser.parse_args()
+    read_args(args, globals())
+    main(input, output,
+         restriction, processes,
+         threshold_num_rest, threshold_span,
+         debug)
