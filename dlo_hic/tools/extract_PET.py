e0bd81e0ee112b395e05297d4d32ab8e7cd284b1
nanguage <nanguage@yahoo.com>
2017-11-10 16:11:35 +0800

    modify extract_PET.py: simplify logic, only suppor SE and delet oututer

--- dlo_hic/tools/extract_PET.py
+++ dlo_hic/tools/extract_PET.py
@@ -21,13 +21,12 @@ import json
 import gzip
 import argparse
 import itertools
-import signal
+import subprocess
 from math import floor
 from copy import copy
 from time import time
 from multiprocessing import Process, Queue, Manager
 
-import regex
 from Bio import SeqIO
 from cutadapt._align import Aligner
 
@@ -43,73 +42,55 @@ def argument_parser():
     parser = argparse.ArgumentParser(
             description="Extract the PET(Pair End Tag) sequences in both left and right side.")
 
-    subparsers = parser.add_subparsers(
-            title="commands",
-            dest="command",
-            metavar="")
-
-    def add_arguments(parser):
-        parser.add_argument("--out1", '-o1',
-                required=True,
-                help="output1: left side PET fastq file")
-
-        parser.add_argument("--out2", '-o2',
-                required=True,
-                help="output2: right side PET fastq file")
-
-        parser.add_argument("--linker-A",
-                dest="linker_a",
-                required=True,
-                help="linker-A")
-
-        parser.add_argument("--linker-B",
-                dest="linker_b",
-                required=True,
-                help="linker-B")
-
-        parser.add_argument("--mismatch",
-                type=int,
-                default=4,
-                help="threshold of linkers base mismatch(and gap open extends) number, default 3")
-
-        parser.add_argument("--rest",
-                type=str,
-                default="A*AGCT*T",
-                help="The sequence of restriction enzyme recognition site, "
-                     "default HindIII: 'A*AGCT*T' ")
-
-        parser.add_argument("--phred",
-                type=int,
-                choices=[33, 64],
-                default=33,
-                help="The Phred score encode offset type, 33 or 64. default 33")
-
-        parser.add_argument("--processes", "-p",
-                type=int,
-                default=1,
-                help="Use how many processes do calculation. default 1")
-
-    # "pair-end" sub command
-    PE_parser = subparsers.add_parser("PE",
-            help="option for pair-end sequencing")
-    PE_parser.add_argument("input1",
-            help="reads1 fastq file")
-    PE_parser.add_argument("input2",
-            help="reads2 fastq file")
-    add_arguments(PE_parser)
-
-    # "single-end" sub command
-    SE_parser = subparsers.add_parser("SE",
-            help="option for single-end sequencing")
-    SE_parser.add_argument("input",
+    parser.add_argument("input",
             help="The input fastq or fastq.gz file.")
-    SE_parser.add_argument("--PET-len",
+
+    parser.add_argument("--PET-len",
             dest="PET_len",
             type=int,
             default=0,
             help="The expected length of PET sequence,"
                  "if PET_len==0 (default) will not limit length.")
-    add_arguments(SE_parser)
+
+    parser.add_argument("--out1", '-o1',
+            required=True,
+            help="output1: left side PET fastq file")
+
+    parser.add_argument("--out2", '-o2',
+            required=True,
+            help="output2: right side PET fastq file")
+
+    parser.add_argument("--linker-A",
+            dest="linker_a",
+            required=True,
+            help="linker-A")
+
+    parser.add_argument("--linker-B",
+            dest="linker_b",
+            required=True,
+            help="linker-B")
+
+    parser.add_argument("--mismatch",
+            type=int,
+            default=4,
+            help="threshold of linkers base mismatch(and gap open extends) number, default 3")
+
+    parser.add_argument("--rest",
+            type=str,
+            default="A*AGCT*T",
+            help="The sequence of restriction enzyme recognition site, "
+                 "default HindIII: 'A*AGCT*T' ")
+
+    parser.add_argument("--phred",
+            type=int,
+            choices=[33, 64],
+            default=33,
+            help="The Phred score encode offset type, 33 or 64. default 33")
+
+    parser.add_argument("--processes", "-p",
+            type=int,
+            default=1,
+            help="Use how many processes do calculation. default 1")
 
     return parser
 
@@ -167,25 +148,13 @@ def open_file(fname, mode='r'):
 def read_fastq(fastq_iter, n=CHUNK_SIZE):
     """
     read fastq file, return records chunk
-    default chunk size 10000
+    default chunk size 1000
     """
     chunk_iter = itertools.islice(fastq_iter, n)
     chunk = list(chunk_iter)
     if len(chunk) == 0:
         raise StopIteration
     return chunk
-
-
-def output(fastq_writer, output_queue):
-    """ output extracted results """
-    def signal_handeler(signal, frame):
-        fastq_writer.write_footer()
-        fastq_writer.handle.close()
-        sys.exit(0) 
-    signal.signal(signal.SIGTERM, signal_handeler)
-    while 1:
-        records = output_queue.get()
-        c = fastq_writer.write_records(records)
 
 
 def add_base_to_PET(PET, base):
@@ -257,51 +226,28 @@ def match_linker(seq, linker, mismatch_threshold, seed_ratio=0.25):
     return span
 
 
-def worker(task_queue, output_queue, counter_queue, linkers, mismatch, rest):
-    """ stream processing(PET extract) task """
-    from Queue import Empty
-    from time import time
-    while 1:
-        all, inter, intra, unmatch = 0,0,0,0 # variables for count reads
-        try:
-            records = task_queue.get(timeout=TIME_OUT)
-        except Empty:
-            break
-        PETs = []
-        for r in records:
-            all += 1
-            seq = str(r.seq)
-            for ltype, linker in linkers.items():
-                span = match_linker(seq, linker, mismatch)
-                if span:
-                    # linker matched
-                    if   (ltype == 'A-A') or (ltype == 'B-B'):
-                        # intra-molcular interaction
-                        intra += 1
-                        PET = extract_PET(r, span, rest)
-                    elif (ltype == 'A-B') or (ltype == 'B-A'):
-                        # inter-molcular interaction
-                        inter += 1
-                    break
-            else:
-                # all linkers can't match
-                unmatch += 1
-        output_queue.put(PETs)
-        counter_queue.put((all, inter, intra, unmatch))
-
-
-def worker_SE(task_queue, output_queue_1, output_queue_2, counter_queue, linkers, mismatch, rest, PET_len):
+def worker_SE(task_queue, out1, out2, phred, counter_queue, linkers, mismatch, rest, PET_len):
     """ stream processing(PET extract) task for SE mode """
     from Queue import Empty
-    from time import time 
+
+    file_out1 = open_file(out1, 'w')
+    file_out2 = open_file(out2, 'w')
+    fastq_writer_1 = fastq_writer(file_out1, phred)
+    fastq_writer_2 = fastq_writer(file_out2, phred)
+
+    fastq_writer_1.write_header()
+    fastq_writer_2.write_header()
+
     while 1:
         all, inter, intra, unmatch = 0,0,0,0 # variables for count reads
         try:
             records = task_queue.get(timeout=TIME_OUT)
         except Empty:
+            fastq_writer_1.write_footer()
+            fastq_writer_1.handle.close()
+            fastq_writer_2.write_footer()
+            fastq_writer_2.handle.close()
             break
-        PETs_1 = []
-        PETs_2 = []
         for r in records:
             all += 1
             seq = str(r.seq)
@@ -313,8 +259,8 @@ def worker_SE(task_queue, output_queue_1, output_queue_2, counter_queue, linkers
                         # intra-molcular interaction
                         intra += 1
                         PET_1, PET_2 = extract_PET(r, span, rest, PET_len, SE=True)
-                        PETs_1.append(PET_1)
-                        PETs_2.append(PET_2)
+                        fastq_writer_1.write_record(PET_1)
+                        fastq_writer_2.write_record(PET_2)
                     elif (ltype == 'A-B') or (ltype == 'B-A'):
                         # inter-molcular interaction
                         inter += 1
@@ -322,128 +268,7 @@ def worker_SE(task_queue, output_queue_1, output_queue_2, counter_queue, linkers
             else:
                 # all linkers can't match
                 unmatch += 1
-        output_queue_1.put(PETs_1)
-        output_queue_2.put(PETs_2)
         counter_queue.put((all, inter, intra, unmatch))
-
-
-def stream_processing(fastq_iter, fastq_writer, processes, linkers, mismatch, rest_site):
-    """ assign stream processing tasks using multiprocessing. """
-    task_queue = Queue()
-    output_queue = Queue()
-
-    # a global queue for count record how many:
-    #   intra-molecular(A-A B-B)
-    #   inter-molecular(A-B B-A)
-    #   unmatchable
-    # reads were captured
-    # element in queue: (all, intra-molecular, inter-molecular, unmatchable)
-    counter_queue = Queue()
-    counter = {
-        'all' : 0,
-        'inter-molecular' : 0,
-        'intra-molecular' : 0,
-        'unmatchable': 0,
-    }
-
-    workers = [Process(target=worker, 
-                         args=(task_queue, output_queue,
-                               counter_queue, linkers, mismatch, rest_site))
-               for i in range(processes)]
-    output_p = Process(target=output, 
-            args=(fastq_writer, output_queue))
-
-    for w in workers:
-        w.start()
-    
-    fastq_writer.write_header()
-    output_p.start()
-
-    try:
-        while 1:
-            task_queue.put(read_fastq(fastq_iter))
-    except StopIteration:
-        pass
-
-    for w in workers:
-        w.join()
-
-    while not output_queue.empty():
-        time.sleep(TIME_OUT)
-    output_p.terminate()
-
-    while not counter_queue.empty():
-        all, inter, intra, un = counter_queue.get()
-        counter['all'] += all
-        counter['inter-molecular'] += inter
-        counter['intra-molecular'] += intra
-        counter['unmatchable'] += un
-
-    return counter
-
-
-def stream_processing_SE(fastq_iter, fastq_writer_1, fastq_writer_2,
-        processes, linkers, mismatch, rest_site, PET_len):
-    """ assign stream processing tasks using multiprocessing. for SE mode """
-    task_queue = Queue()
-    output_queue_1 = Queue()
-    output_queue_2 = Queue()
-
-    # a global queue for count record how many:
-    #   intra-molecular(A-A B-B)
-    #   inter-molecular(A-B B-A)
-    #   unmatchable
-    # reads were captured
-    # element in queue: (all, intra-molecular, inter-molecular, unmatchable)
-    counter_queue = Queue()
-    counter = {
-        'all' : 0,
-        'inter-molecular' : 0,
-        'intra-molecular' : 0,
-        'unmatchable': 0,
-    }
-
-    workers = [Process(target=worker_SE, 
-                         args=(task_queue, output_queue_1, output_queue_2,
-                               counter_queue, linkers, mismatch, rest_site, PET_len))
-               for i in range(processes)]
-    output_p_1 = Process(target=output, 
-            args=(fastq_writer_1, output_queue_1))
-    output_p_2 = Process(target=output, 
-            args=(fastq_writer_2, output_queue_2))
-
-    for w in workers:
-        w.start()
-    
-    fastq_writer_1.write_header()
-    output_p_1.start()
-    fastq_writer_2.write_header()
-    output_p_2.start()
-
-    try:
-        while 1:
-            task_queue.put(read_fastq(fastq_iter))
-    except StopIteration:
-        pass
-
-    for w in workers:
-        w.join()
-
-    while not output_queue_1.empty():
-        time.sleep(TIME_OUT)
-    output_p_1.terminate()
-    while not output_queue_2.empty():
-        time.sleep(TIME_OUT)
-    output_p_2.terminate()
-
-    while not counter_queue.empty():
-        all, inter, intra, un = counter_queue.get()
-        counter['all'] += all
-        counter['inter-molecular'] += inter
-        counter['intra-molecular'] += intra
-        counter['unmatchable'] += un
-
-    return counter
 
 
 def fastq_iter(file_in, phred):
@@ -464,31 +289,27 @@ def fastq_writer(file_out, phred):
     return fastq_writer
 
 
-def mainPE(input1, input2, out1, out2,
-        linekr_a, linker_b,
-        mismatch, rest, phred, processes):
-    # parse restriction enzyme site
-    rest_site = parse_rest(rest)
-
-    # load linkers
-    linkers = load_linkers(linekr_a, linker_b)
-    log_linkers(linkers)
-
-    with open_file(input1) as file_in1, open_file(input2) as file_in2,\
-         open_file(out1, 'w') as file_out1, open_file(out2, 'w') as file_out2:
-
-        fastq_iter_1 = fastq_iter(file_in1, phred)
-        fastq_iter_2 = fastq_iter(file_in2, phred)
-        fastq_writer_1 = fastq_writer(file_out1, phred)
-        fastq_writer_2 = fastq_writer(file_out2, phred)
-
-        counts_1 = stream_processing(fastq_iter_1, fastq_writer_1,
-            processes, linkers, mismatch, rest_site)
-
-        counts_2 = stream_processing(fastq_iter_2, fastq_writer_2,
-            processes, linkers, mismatch, rest_site)
-    
-    return (counts_1, counts_2)
+def counts_reads_type(counter_queue):
+    """
+    count all reads type
+      intra-molecular(A-A B-B)
+      inter-molecular(A-B B-A)
+      unmatchable
+    element in queue: (all, intra-molecular, inter-molecular, unmatchable)
+    """
+    counter = {
+        'all' : 0,
+        'inter-molecular' : 0,
+        'intra-molecular' : 0,
+        'unmatchable': 0,
+    }
+    while not counter_queue.empty():
+        all, inter, intra, un = counter_queue.get()
+        counter['all'] += all
+        counter['inter-molecular'] += inter
+        counter['intra-molecular'] += intra
+        counter['unmatchable'] += un
+    return counter
 
 
 def mainSE(input, out1, out2,
@@ -501,16 +322,43 @@ def mainSE(input, out1, out2,
     linkers = load_linkers(linekr_a, linker_b)
     log_linkers(linkers)
 
-    with open_file(input) as file_in1, open_file(input) as file_in2,\
-         open_file(out1, "w") as file_out1, open_file(out2, 'w') as file_out2:
+    task_queue = Queue()
+    counter_queue = Queue() # a global queue for count record how many:
 
-        fq_iter = fastq_iter(file_in1, phred)
-        fastq_writer_1 = fastq_writer(file_out1, phred)
-        fastq_writer_2 = fastq_writer(file_out2, phred)
+    workers = [Process(target=worker_SE, 
+                         args=(task_queue, out1+".tmp.%d"%i, out2+".tmp.%d"%i, phred,
+                               counter_queue, linkers, mismatch, rest_site, PET_len))
+               for i in range(processes)]
 
-        counts = stream_processing_SE(fq_iter, fastq_writer_1, fastq_writer_2,
-            processes, linkers, mismatch, rest_site, PET_len)
-    
+    for w in workers:
+        w.start()
+
+    # put reads in queue
+    with open_file(input) as file_in:
+        fq_iter = fastq_iter(file_in, phred)
+        try:
+            while 1:
+                task_queue.put(read_fastq(fq_iter))
+        except StopIteration:
+            pass
+
+    # wait subprocesses end
+    for w in workers:
+        w.join()
+
+    # merge all tmp files
+    tmpfiles_1 = [out1+".tmp.%d"%i for i in range(processes)]
+    tmpfiles_2 = [out2+".tmp.%d"%i for i in range(processes)]
+    cmd = "cat " + " ".join(tmpfiles_1) + " > " + out1
+    subprocess.check_call(cmd, shell=True)
+    cmd = "cat " + " ".join(tmpfiles_2) + " > " + out1
+    subprocess.check_call(cmd, shell=True)
+    cmd = "rm " + " ".join(tmpfiles_1 + tmpfiles_2)
+    subprocess.check_call(cmd, shell=True)
+
+    # count reads type
+    counts = counts_reads_type(counter_queue)
+
     return counts
 
 
@@ -519,16 +367,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     read_args(args, globals())
-    
-    if command == 'SE':
-        counts = mainSE(input, out1, out2,
-                linker_a, linker_b,
-                mismatch, rest, phred, processes, PET_len)
-        log_counts(counts)
 
-    elif command == 'PE':
-        counts_1, counts_2 = mainPE(input1, input2, out1, out2,
-                linker_a, linker_b,
-                mismatch, rest, phred, processes)
-        log_counts(counts_1)
-        log_counts(counts_2)
+    counts = mainSE(input, out1, out2,
+            linker_a, linker_b,
+            mismatch, rest, phred, processes, PET_len)
+
+    log_counts(counts)
