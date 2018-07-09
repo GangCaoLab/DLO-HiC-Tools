4405efb5f5c848fab1d180a4740a8c25eceb4c60
nanguage <nanguage@yahoo.com>
2018-07-09 16:21:47 +0800

    modify api

--- dlo_hic/api.py
+++ dlo_hic/api.py
@@ -54,7 +54,7 @@ class DLO_HiC(object):
         self.processes = processes
 
         # set logger
-        self.log = logging.getLogger("DLO_HiC<%s/%s>"%(workdir, prefix))
+        self.log = logging.getLogger("DLO_HiC <%s>"%(join(workdir, prefix)))
         if log_file == '-':
             handler = logging.StreamHandler(sys.stderr)
         else:
@@ -68,23 +68,31 @@ class DLO_HiC(object):
         self.log.info("processes: {}".format(processes))
 
     def __check_required(self, para_name, kwargs):
-        attr_name = para_name
-        if not hasattr(self, attr_name):
+        if not hasattr(self, para_name):
             if para_name not in kwargs:
                 msg = "parameter \"{}\" is need, you must provide it as an argument, or set it to attribute.".format(para_name)
                 raise ValueError(msg)
             else:
-                setattr(self, attr_name, kwargs[para_name])
+                setattr(self, para_name, kwargs[para_name])
 
     def __check_optional(self, para_name, kwargs, default):
-        attr_name = para_name
-        if not hasattr(self, attr_name):
+        if not hasattr(self, para_name):
             if para_name not in kwargs:
                 msg = "parameter \"{}\" is not specified, set to default value: {}".format(para_name, default)
                 self.log.info(msg)
-                setattr(self, attr_name, default)
+                setattr(self, para_name, default)
             else:
-                setattr(self, attr_name, kwargs[para_name])
+                setattr(self, para_name, kwargs[para_name])
+
+    def __check_output(self, para_name, kwargs, default):
+        if not hasattr(self, para_name):
+            if para_name not in kwargs:
+                msg = "parameter \"{}\" is not specified, set to default value: {}".format(para_name, default)
+                self.log.info(msg)
+                output = default
+            else:
+                output = kwargs[para_name]
+        return output
 
     def __mkdir(self, dirname):
         dir_ = join(self.workdir, dirname)
@@ -93,25 +101,60 @@ class DLO_HiC(object):
         return dir_
 
     def extract_PET(self, *args, **kwargs):
+        """
+        Extract PETs from fastq file.
+
+        Parameters
+        ----------
+        fastq : str
+            Path to input fastq file.
+
+        PET1_file : str
+            Path to output PET file 1.
+        
+        PET2_file : str
+            Path to output PET file 2.
+        
+        linker_a : str
+            Sequence of Linker A.
+
+        linker_b : str
+            Sequence of Linker B.
+
+        mismatch : int
+            Threshold of linkers base mismatch(and gap open extends) number.
+
+        rest_pattern : str
+            The sequence of restriction enzyme recognition site.
+            Example: 'A*AGCT*T'
+
+        PET_len : int
+            The expected length of PET sequence,
+            if 0 (default) will not limit length,
+            NOTE: Adapter for sequencing must be trimmed in this situation.
+
+        log_file : str
+            Sperate log file record reads count information.
+        """
         from dlo_hic.tools.main_process.extract_PET import main
 
         self.__check_required('fastq', kwargs)
         self.__check_required('linker_a', kwargs)
         self.__check_optional('linker_b', kwargs, None)
         self.__check_optional('mismatch', kwargs, DLO_HiC.DEFAULT_MISMATCH)
-        self.__check_required('rest', kwargs)
+        self.__check_required('rest_pattern', kwargs)
         self.__check_optional('phred', kwargs, DLO_HiC.DEFAULT_PHRED)
         self.__check_optional('PET_len', kwargs, DLO_HiC.DEFAULT_PET_LEN)
 
         dir_ = self.__mkdir('01-extpet')
 
-        PET1_file = join(dir_, self.prefix+'.pet1.fq')
-        PET2_file = join(dir_, self.prefix+'.pet2.fq')
-        PET_log = join(dir_, self.prefix+'.pet.log')
+        PET1_file = self.__check_output('PET1_file', kwargs, join(dir_, self.prefix+'.pet1.fq'))
+        PET2_file = self.__check_output('PET2_file', kwargs, join(dir_, self.prefix+'.pet2.fq'))
+        PET_log = self.__check_output('PET_log', kwargs, join(dir_, self.prefix+'.pet.log'))
 
         main(self.fastq, PET1_file, PET2_file,
              self.linker_a, self.linker_b,
-             self.mismatch, self.rest, self.phred, self.processes, self.PET_len,
+             self.mismatch, self.rest_pattern, self.phred, self.processes, self.PET_len,
              PET_log)
 
         setattr(self, 'PET1_file', PET1_file)
@@ -119,6 +162,29 @@ class DLO_HiC(object):
         setattr(self, 'PET_log', PET_log)
 
     def build_bedpe(self, *args, **kwargs):
+        """
+        Build bedpe file from two PET fastq file.
+
+        Parameters
+        ----------
+        PET1_file : str
+            Path to PET fastq file 1.
+
+        PET2_file : str
+            Path to PET fastq file 2.
+
+        bed_file : str
+            Path to output bedpe file.
+
+        bwa_index : str
+            Path to bwa index prefix.
+
+        mapq : int
+            The mapq threshold used to filter mapped records. default 1(for fetch unique mapping)
+
+        bwa_log_file : str
+            Path to bwa log file.
+        """
         from dlo_hic.tools.main_process.build_bedpe import main
 
         self.__check_required('PET1_file', kwargs)
@@ -128,8 +194,8 @@ class DLO_HiC(object):
 
         dir_ = self.__mkdir('02-bedpe')
 
-        bedpe_file = join(dir_, self.prefix+'.uniq.bedpe')
-        bwa_log_file = join(dir_, self.prefix+'.bwa.log')
+        bedpe_file  = self.__check_output('bedpe_file', kwargs, join(dir_, self.prefix+'.uniq.bedpe'))
+        bwa_log_file = self.__check_output('bwa_log_file', kwargs, join(dir_, self.prefix+'.bwa.log'))
 
         main('fastq', self.PET1_file, self.PET2_file, bedpe_file,
              self.processes, self.bwa_index, self.mapq, bwa_log_file)
@@ -138,6 +204,26 @@ class DLO_HiC(object):
         setattr(self, 'bwa_log_file', bwa_log_file)
 
     def noise_reduce(self, *args, **kwargs):
+        """
+        Remove DLO-HiC noise (self-ligation).
+
+        Parameters
+        ----------
+        bedpe_file : str
+            Path to input bedpe file.
+
+        nr_file : str
+            Path to output file.
+        
+        restriction_site_file : str
+            Path to bed file which recorded the position of all restriction sites.
+
+        threshold_num_rest : int
+            Threshold of number of restriction sites with pair, default 1.
+
+        threshold_span : int
+            Threshold of pair span, default 50.
+        """
         from dlo_hic.tools.main_process.noise_reduce import main
 
         self.__check_required('bedpe_file', kwargs)
@@ -146,7 +232,8 @@ class DLO_HiC(object):
         self.__check_optional('threshold_span', kwargs, DLO_HiC.DEFAULT_THRESH_SPAN)
 
         dir_ = self.__mkdir('03-nr')
-        nr_file = join(dir_, self.prefix+'.nr.bedpe')
+
+        nr_file = self.__check_output('nr_file', kwargs, join(dir_, self.prefix+'.nr.bedpe'))
 
         main(self.bedpe_file, nr_file, 
              self.restriction_site_file, self.processes,
@@ -155,23 +242,54 @@ class DLO_HiC(object):
         setattr(self, 'nr_file', nr_file)
 
     def remove_reduncancy(self, *args, **kwargs):
+        """
+        Remove the redundancy within pairs.
+
+        Parameters
+        ----------
+        nr_file : str
+            Path to input bedpe file.
+
+        rr_file : str
+            Path to output file.
+
+        threshold_rr_distance : str
+            The threshold of distance, if pairs both ends's distance,
+            small than this at same time, consider them as the redundancy.
+            default 0(exactly same).
+        """
         from dlo_hic.tools.main_process.remove_redundancy import main
 
         self.__check_required('nr_file', kwargs)
         self.__check_optional('threshold_rr_distance', kwargs, DLO_HiC.DEFAULT_THRESH_RR_DISTANCE)
         dir_ = self.__mkdir('04-rr')
-        rr_file = join(dir_, self.prefix+'.rr.bedpe')
+        rr_file = self.__check_output('rr_file', kwargs, join(dir_, self.prefix+'.rr.bedpe'))
         main(self.nr_file, rr_file, self.threshold_rr_distance)
         setattr(self, 'rr_file', rr_file)
 
     def bedpe2pairs(self, *args, **kwargs):
+        """
+        Transform bedpe format file to pairs format, and index it use pairix
+
+        Parameters
+        ----------
+        rr_file : str
+            Path to input bedpe file.
+
+        pairs_file : str
+            Path to output pairs file.
+
+        keep_raw_pairs : bool
+            Keep non compressed pairs file, 
+            if you need create .hic file use --keep option.
+        """
         from dlo_hic.tools.main_process.bedpe2pairs import main
 
         self.__check_required('rr_file', kwargs)
         self.__check_optional('keep_raw_pairs', kwargs, DLO_HiC.KEEP_RAW_PAIRS)
 
         dir_ = self.__mkdir('05-rr')
-        pairs_file = join(dir_, self.prefix+".pairs")
+        pairs_file = self.__check_output('pairs_file', kwargs, join(dir_, self.prefix+".pairs"))
 
         main(self.rr_file, pairs_file, self.keep_raw_pairs)
 
