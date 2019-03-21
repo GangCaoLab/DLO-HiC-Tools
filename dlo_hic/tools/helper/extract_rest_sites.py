df565c89fb07a48942027de1aee319d975bad40b
nanguage <nanguage@yahoo.com>
2019-03-21 15:15:03 +0800

    update, add hdf5 format rest sites

--- dlo_hic/tools/helper/extract_rest_sites.py
+++ dlo_hic/tools/helper/extract_rest_sites.py
@@ -9,8 +9,9 @@ import multiprocessing as mp
 
 import pyfaidx
 import click
+import h5py
+import numpy as np
 
-from dlo_hic.utils import read_args
 from dlo_hic.utils import reverse_complement as rc
 from dlo_hic.utils.wrap.tabix import sort_bed6, index_bed6
 
@@ -22,6 +23,7 @@ def worker(task_queue, output_queue, rest, fasta):
     rest = str(rest)
     rest_rc = rc(rest)
     faidx = pyfaidx.Fasta(fasta)
+
     while 1:
         chr_ = task_queue.get()
         if chr_ is None:
@@ -30,28 +32,53 @@ def worker(task_queue, output_queue, rest, fasta):
 
         seq = faidx[chr_][:].seq # read sequence
 
+        out_chunk = [] 
         for match in re.finditer(rest, seq, re.IGNORECASE):
-            output_queue.put(
-                (chr_, str(match.start()), str(match.end()), '.', '0', '+')
+            out_chunk.append(
+                match.start()
             )
+        output_queue.put( (chr_, '+', out_chunk) )
 
         if rest_rc != rest: # find reverse complement restriction site
+            out_chunk = []
             for match in re.finditer(rest_rc, seq, re.IGNORECASE):
-                output_queue.put(
-                    (chr_, str(match.start()), str(match.end()), '.', '0', '-')
+                out_chunk.append(
+                    match.start()
                 )
+            output_queue.put( (chr_, '-', out_chunk) )
 
 
-def outputer(output_file, output_queue):
+def outputer(output, out_fmt, rest_seq, output_queue):
     """ output extracted results """
-    while 1:
-        record = output_queue.get()
-        if record is None:
-            log.debug("Process-output done.")
-            output_file.flush()
-            break
-        line = "\t".join(record) + "\n"
-        output_file.write(line)
+    if out_fmt == 'tab':
+        with open(output, 'w') as f:
+            while 1:
+                out_tupl = output_queue.get()
+                if out_tupl is None:
+                    log.debug("Process-output done.")
+                    f.flush()
+                    break
+                chr_, strand, out_chunk = out_tupl
+                for start in out_chunk:
+                    # BED6: [chr, start, end, name, score, strand]
+                    out_itms = [chr_, str(start), str(start + len(rest_seq)), ".", "0", strand]
+                    out_line = "\t".join(out_itms) + "\n"
+                    f.write(out_line)
+    elif out_fmt == 'hdf5':
+        output = output + '.hdf5' if not output.endswith('.hdf5') else output
+        with h5py.File(output, 'w') as f:
+            f.create_group("chromosomes")
+            f.attrs['rest_seq'] = rest_seq
+            while 1:
+                out_tupl = output_queue.get()
+                if out_tupl is None:
+                    log.debug("Process-output done.")
+                    f.flush()
+                    break
+                chr_, _, out_chunk = out_tupl
+                f.create_dataset("chromosomes/"+chr_, data=np.array(out_chunk, dtype=np.int))
+    else:
+        raise NotImplementedError("output format only support tab and hdf5.")
 
 
 @click.command(name="extract_rest_sites")
@@ -59,9 +86,14 @@ def outputer(output_file, output_queue):
 @click.option("--rest-seq", "-r", "rest", required=True,
     help="The sequence of restriction site")
 @click.argument("output", nargs=1)
+@click.option("--output-format", "-f",
+    type=click.Choice(['tab', 'hdf5']),
+    default='tab',
+    show_default=True,
+    help="The file output format.")
 @click.option("--processes", "-p", default=1,
     help="Use how many processes to run. default 1")
-def _main(fasta, rest, output, processes):
+def _main(fasta, rest, output, output_format, processes):
     """ Extract all restriction sites from fasta file, save to BED6 file format. """
 
     log.info("Extract restriction sites %s from %s"%(rest, fasta))
@@ -73,7 +105,7 @@ def _main(fasta, rest, output, processes):
     chrs = faidx.keys()
 
     task_queue   = mp.Queue()
-    output_queue = mp.Queue()
+    output_queue = mp.Queue(maxsize=processes)
 
     processes = min(processes, len(chrs))
 
@@ -86,28 +118,29 @@ def _main(fasta, rest, output, processes):
     for w in workers: # put end flag to task queue
         task_queue.put(None)
 
-    with tempfile.NamedTemporaryFile(mode='w') as tmp:
-        output_p = mp.Process(target=outputer, args=(tmp, output_queue))
+    output_p = mp.Process(target=outputer, args=(output, output_format, rest, output_queue))
 
-        # launch processes
-        for w in workers:
-            w.start()
-        output_p.start()
+    # launch processes
+    for w in workers:
+        w.start()
+    output_p.start()
 
-        log.info("%d workers spawned for extract restriction sites"%len(workers))
+    log.info("%d workers spawned for extract restriction sites"%len(workers))
 
-        # wait all tasks done
-        for w in workers:
-            w.join()
+    # wait all tasks done
+    for w in workers:
+        w.join()
 
-        # put end flag to output queue
-        output_queue.put(None)
+    # put end flag to output queue
+    output_queue.put(None)
 
-        output_p.join() # wait all result outputed
+    output_p.join() # wait all result outputed
 
+    if output_format == 'tab':
         # sort output bed file
         log.info("sorting bed file ...")
-        sort_bed6(tmp.name, output)
+        sort_bed6(output, output+'.sort')
+        subprocess.check_call(["mv", output+'.sort', output])
         log.info("building tabidx...")
         index_bed6(output)
         subprocess.check_call(["rm", output])
